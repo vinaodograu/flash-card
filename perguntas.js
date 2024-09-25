f07c46b7dc5dca1e045cb1b7a61acd9ca876db1d
vinaodograu <126502507+vinaodograu@users.noreply.github.com>
2024-09-25 10:58:22 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -11,18 +11,13 @@ criaCartao(
 )
 
 criaCartao(
-  'Programação',
+    'Programação',
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
-cartao.innerHTML = `
-<div class="cartao__conteudo">
-<h3>Programação</h3>
-<div class="cartao__conteudo__pergunta">
-        <p>O que é JavaScript?</p>
-</div>
-<div class="cartao__conteudo__resposta">
-        <p>O JavaScript é uma linguagem de programação</p>
-</div>
-</div>
-`
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz OI em inglês?',
+    'Oi em ingles é HI (RAI)'
+) 
